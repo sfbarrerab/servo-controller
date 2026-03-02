aa6de9c93108e9037d5381773449e3d6dc41a758
sfbarrerab <sfbarrerab@unal.edu.co>
2026-03-02 19:17:56 +0100

    Include comments on the memory address handler

--- src/memory_mgmt.cpp
+++ src/memory_mgmt.cpp
@@ -1,13 +1,17 @@
 #include <memory_mgmt.h>
 
+// Each servo reserves 4 bytes
+// First position occupies the first 2 bytes...
 int getFirstAddr(int servoId) {
   return EEPROM_BASE_ADDR + servoId * 4;
 }
 
+// ... second position occupies the next 2 bytes after first position
 int getSecondAddr(int servoId) {
   return EEPROM_BASE_ADDR + servoId * 4 + 2;
 }
 
+// Read the stored values in the memory, and if empty, initialize with default values 600,900
 void readEEPROM() {
   for (int i = 0; i < NUMBER_OF_SERVOS; i++) {
 
@@ -17,7 +21,7 @@ void readEEPROM() {
     EEPROM.get(getFirstAddr(i), firstPos);
     EEPROM.get(getSecondAddr(i), secondPos);
 
-    // Validate values (important!)
+    // Validate values
     if (firstPos < PULSE_MIN || firstPos > PULSE_MAX)
       firstPos = 600;
 
@@ -29,6 +33,7 @@ void readEEPROM() {
   }
 }
 
+// Store the values in the memory
 void writeEEPROM(int servoId, int firstPos, int secondPos) {
   EEPROM.put(getFirstAddr(servoId), firstPos);
   EEPROM.put(getSecondAddr(servoId), secondPos);
